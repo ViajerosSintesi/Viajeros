4924f5c86e9a7cd2cfecccd309d1f12478184c1f
OpenShift guest <mascarantitxema@gmail.com>
2014-05-09 07:00:53 -0400

    prova con mongoclient

--- provaWiki.php
+++ provaWiki.php
@@ -1,8 +1,11 @@
 <?php
 
+$con = new MongoClient("mongodb://txemens:h0lita@ds043329.mongolab.com:43329/viajeros");
+var_dump($con);
+/*
 $json = file_get_contents('http://es.wikipedia.org/w/api.php?format=php&action=query&titles=India&prop=revisions&rvprop=content');
 
 echo $json;
-
+*/
 
 ?>
