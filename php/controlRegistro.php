fce6921d9761c2c72070421ba6d3cd73cba5373a
Txema!! <mascarantitxema@gmail.com>
2014-05-10 13:21:01 -0400

    actualizacion!

--- php/controlRegistro.php
+++ php/controlRegistro.php
@@ -1,24 +1,21 @@
 <?php
 	require_once("./clases/UserClass.php");
 
-	//if(filter_has_var(INPUT_POST, "user")){
+	if(filter_has_var(INPUT_POST, "user") && filter_has_var(INPUT_POST, "mail") && filter_has_var(INPUT_POST, "pass") && filter_has_var(INPUT_POST, "edad")){
 
 		$newUser = new User();
 
-		$mail = "mascarantitxema@gmail.com";
-		$username = "txemens";
-		$password = "h0lita";
-		$edad = "24";
+		$mail = filter_input(INPUT_POST, "mail");
+		$username = filter_input(INPUT_POST, "user");
+		$password = filter_input(INPUT_POST, "pass");
+		$edad = filter_input(INPUT_POST, "edad");
 
 		$newUser->setUsername($username);
 		$newUser->setId($mail);
 		$newUser->setPassword($password);
 		$newUser->setEdad($edad);
-
 		$newUser->guardarUser();
-
-
-	//}
+	}
 	
 
 
