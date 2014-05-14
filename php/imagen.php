1edd39bb19c5368b786f29259cc4469402797cfe
Txema!! <mascarantitxema@gmail.com>
2014-05-14 12:16:05 -0400

    unas modificaciones

---
+++ php/imagen.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ * en la tabla imagenes tiene el formato:
+ * 
+ * {
+ *   "_id": id,
+ *   "ruta": ruta a la imagen,
+ *   "cod_ciudad": codigo en la ciudad que ha sido hecha
+ *   "cod_usuario": id del usuario que ha tomado la foto
+ * }
+ * 
+ **/
+
+?>
