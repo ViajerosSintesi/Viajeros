ccbc7d957f92f917179ebd400520ad725dd950e8
Lui86 <l.ruiz86@gmail.com>
2014-06-03 01:21:04 +0200

    cositas

--- ciudad.php
+++ ciudad.php
@@ -88,16 +88,17 @@ $(function(){
 <body>
 <div id="wrap">
 	<div id="menu">
-		<button id="hide-show-menu">Barra men&uacute; <img src="img/33.png" id="ico-menu"></button>
+		<button id="hide-show-menu" title="Ocultar/mostrar barra de men&uacute;">Barra men&uacute; <img src="img/33.png" id="ico-menu"></button>
 		<div id="barra-menu">
 			<input type="text" id="buscar" title="Buscar ciudad, pa&iacute;s">
 			<input type="hidden" value="" id="id" />
 			<ul>
-				<li><a href="perfil.php">Perfil</a></li>
-				<li><a href="list-pais.php">Paises</a></li>
-				<li><a href="mapa.php">Mapa</a></li>
-				<li><form action="php/controlLogin.php" method="post">
-						<input type="submit" name="destroySession" id="destroySession" value="Cerrar session" /> 
+				<li><a href="#" id="miUbicacion" title="Obterner ubicaci&oacute;n"><img src="img/161.png"></a></li>
+				<li><a href="perfil.php" title="Ir a perfil">Perfil</a></li>
+				<li><a href="list-pais.php" title="Lista de paises">Paises</a></li>
+				<li><a href="mapa.php" title="Muestra el mapa">Mapa</a></li>
+				<li><form action="php/controles/controlLogin.php" method="post">
+						<input type="submit" name="destroySession" id="destroySession" value="Cerrar session" title="Cambiar foto de perfil" /> 
 					</form></li>
 			</ul>
 		</div>
