1944fb18942a60c0f913c46962fe9dca7ce3e42c
Txema!! <mascarantitxema@gmail.com>
2014-06-03 04:55:11 -0400

    mas respuestas

---
+++ php/clases/RespuestaClass.php
@@ -0,0 +1,67 @@
+<?php
+
+
+ require_once("ClassMongoClient.php");
+/**
+* 
+*/
+class Respuesta{
+	
+	private $id = null;
+	private $user = null;
+	private $idPregunta = null;
+      private $respuesta = null;
+	private $idSitio = null;
+	private $bbdd;
+	
+	function __construct($bbdd){
+	      $this->bbdd = new DBMongo($bbdd);
+	}
+	
+      public function getId(){return $this->id;}
+      public function setId($id){$this->id = $id;}
+      public function getUser(){return $this->user;}
+      public function setUser($user){$this->user = $user;}
+      public function getIdPregunta(){return $this->idPregunta;}
+      public function setIdPregunta($idPregunta){$this->idPregunta = $idPregunta;}
+      public function getRespuesta(){return $this->respuesta;}
+      public function setRespuesta($respuesta){$this->respuesta = $respuesta;}
+      public function getIdSitio(){return $this->idSitio;}
+      public function setIdSitio($idSitio){$this->idSitio = $idSitio;}
+
+      
+      public function cogeValoresSegunId(){
+	      $queryForId = array('_id' => $this->id);
+      	if($this->bbdd->contar($queryForId)){
+	      	$respuesta = $this->bbdd->findOneCollection($queryForId);
+		      $this->id = $respuesta['_id'];
+		      $this->usuario = $respuesta['idUsu'];
+	            $this->idSitio= (isset($respuesta['idCiu']))? $respuesta['idCiu'] : $respuesta['idPais'];
+	            $this->idPregunta= $respuesta['idPregunta'];
+	     }
+      }
+      public function borrarRespuesta(){
+            $queryForDelete = array("_id"=>new MongoId($this->id));
+            return  $this->bbdd->eliminar($queryForDelete);
+      }
+      public function devolverDeLaPregunta($tipo){
+            if($tipo == "Pais")
+                  $query = array("idPais" =>new MongoId($this->idSitio));
+            elseif($tipo = "Ciudad")
+                  $query = array('idCiu'=>new MongoId($this->idSitio));
+            $queryForInsert["idPregunta"] => $this->idPregunta;
+            //var_dump($this->bbdd->findCollection($query));
+            //$queryForView = array("_id"=>false, "valor"=>true);
+            return  $this->bbdd->findCollection($query);
+      }
+      public function insertarRespuesta($tipo){
+            $queryForInsert = array("idUsu"=>$this->user, 
+                              "pregunta"=>$this->pregunta);
+            if($tipo == "Ciudad") $queryForInsert["idCiu"]= $this->idSitio;
+            elseif($tipo == "Pais") $queryForInsert["idPais"]= $this->idSitio;
+            $queryForInsert["idPregunta"] => $this->idPregunta;
+            return $this->bbdd->insertar($queryForInsert);
+      }
+}
+
+?>
---
+++ php/controles/controlPregunta.php
@@ -0,0 +1,59 @@
+<?php
+
+ 
+require_once("../clases/ValoracionClass.php");
+require_once("../clases/PreguntaClass.php");
+require_once("../clases/UserClass.php");
+  
+if(filter_has_var(INPUT_GET, "ciudad") && filter_has_var(INPUT_GET, "verPreguntas")){
+  $ciudad = filter_input(INPUT_GET, "ciudad");
+  $userId = filter_input(INPUT_GET, "userId");
+  $tipo = filter_input(INPUT_GET, "verPreguntas");
+  $preguntaCiudad = new Comment("pregunta".$tipo);
+  
+  $preguntaCiudad->setIdSitio($ciudad);
+  $preguntas = $cCiudad->devolverDelSitio($tipo);
+  
+  $valoracionPregunta = new Valoracion("valoracionpregunta");
+  $valoracionPregunta->setUser($userId);
+  $user = new User();
+  for($i=0;$i<count($preguntas);$i++){
+      $valoracionPregunta->setObject($preguntas[$i]["_id"]->{'$id'});
+      $valoraciones = $valoracionPregunta->verValoraciones();
+      $preguntas[$i]["valorNeg"]=0;
+      $preguntas[$i]["valorPos"]=0;
+      
+      for($x = 0; $x<count($valoraciones); $x++){
+            if($valoraciones[$x]["valor"] == 1) $preguntas[$i]["valorNeg"]++;
+            elseif($valoraciones[$x]["valor"] == 2)$preguntas[$i]["valorPos"]++; 
+      }
+      $preguntas[$i]["valorDelUser"] = $valoracionPregunta->verValoracionDelUsuario(); 
+      $user->setId($preguntas[$i]["idUsu"]);
+      $user->cogeValoresSegunId();
+      $preguntas[$i]["nombreDelUser"] = $user->getUsername();
+      $preguntas[$i]["imgPerfilUser"] = $user->getImgPerfil();
+  }
+  
+  //echo "<pre>";
+  //var_dump($preguntas);
+  
+  echo json_encode($preguntas);
+      
+}
+
+if(filter_has_var(INPUT_GET,"ciudad")&& filter_has_var(INPUT_GET, "insertarPregunta")){
+      $ciudad = filter_input(INPUT_GET, "ciudad");
+      $userId = filter_input(INPUT_GET, "userId");
+      $tipo = filter_input(INPUT_GET, "insertarPregunta");
+      $preguntaText = filter_input(INPUT_GET, "pregunta");
+      
+      $pregunta= new Comment("pregunta".$tipo);
+      
+      $pregunta->setUser($userId);
+      $pregunta->setIdSitio(new MongoId($ciudad));
+      $pregunta->setPregunta($preguntaText);
+      
+      echo json_encode($pregunta->insertarPregunta($tipo));
+}
+
+?>
