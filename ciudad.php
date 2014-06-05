7af32c8594c0f59947d862e2fe56740d88e0faff
Txema!! <mascarantitxema@gmail.com>
2014-06-05 10:55:38 -0400

    uea

--- ciudad.php
+++ ciudad.php
@@ -41,6 +41,7 @@ $(function(){
 			$("#contenido").load("php/ciudad/ciudad-info.php?ciudad=<?php echo $ciudad;?>");
 			return false;
 		});
+		
 		$("#fotos").click(function(){
 			//fotoCiudad();
 			$("#contenido").load("php/ciudad/ciudad-foto.php?ciudad=<?php echo $ciudad;?>");
@@ -49,6 +50,11 @@ $(function(){
 		$("#comentarios").click(function(){
 			//comentariosPais();
 			$("#contenido").load("php/ciudad/ciudad-comentarios.php?ciudad=<?php echo $ciudad;?>");
+			return false;
+		});
+		$("#preguntas").click(function(){
+			//comentariosPais();
+			$("#contenido").load("php/ciudad/ciudad-preguntas.php?ciudad=<?php echo $ciudad;?>");
 			return false;
 		});
 		$("#ubicacion").click(function(){
@@ -69,7 +75,7 @@ $(function(){
 });
       function valoraciones(){
             $.getJSON("php/controles/controlValoracionCiudad.php",{"ciudad":"<?php echo $ciudad;?>", "verValor":"1"}, function(data){
-                  if(data!=null)$("#valoracion-lugar").html(data);
+                  if(data!=null)$("#valoracionCiudad").html("Nota:"+data);
             });
             var queryForValoracionUsuario = {"ciudad":"<?php echo $ciudad;?>","userId":"<?php echo $userId;?>", "verValorUsuario":"1"};
             $.getJSON("php/controles/controlValoracionCiudad.php",queryForValoracionUsuario, function(data){
@@ -100,10 +106,10 @@ $(function(){
 			<input type="text" id="buscar" title="Buscar ciudad, pa&iacute;s">
 			<input type="hidden" value="" id="id" />
 			<ul>
-				<li><a href="#" id="miUbicacion" title="Obterner ubicaci&oacute;n" alt="Obterner ubicaci&oacute;n"><img src="img/161.png"></a></li>
-				<li><a href="perfil.php" title="Ir a perfil" alt="Ir a perfil">Perfil</a></li>
-				<li><a href="list-pais.php" title="Lista de paises" alt="Lista de paises">Paises</a></li>
-				<li><a href="mapa.php" title="Muestra el mapa" alt="Muestra el mapa">Mapa</a></li>
+				<li><a href="#" id="miUbicacion" title="Obterner ubicaci&oacute;n"><img src="img/161.png"></a></li>
+				<li><a href="perfil.php" title="Ir a perfil">Perfil</a></li>
+				<li><a href="list-pais.php" title="Lista de paises">Paises</a></li>
+				<li><a href="mapa.php" title="Muestra el mapa">Mapa</a></li>
 				<li><form action="php/controles/controlLogin.php" method="post">
 						<input type="submit" name="destroySession" id="destroySession" value="Cerrar session" title="Cambiar foto de perfil" /> 
 					</form></li>
@@ -111,49 +117,47 @@ $(function(){
 		</div>
 	</div>
 	<div id="header">
-		<img src="img/logo.png" alt="Logo corporativo">
+		<img src="img/logo.png">
+		<h1 id="nombreCity"></h1>
 	</div>
 	<div id="contenedor">
-		<div id="cabecera">
-			<!-- <img src="img/home_img1.jpg"> -->
-			<span id="valoracion-lugar"></span>
-			<div id="subcabecera-lugar">
-				<div id="titulo-lugar">
-					<?php $nombreCiudad = obtenerInfoCiudad($ciudad);?>
-					<h1><?php echo $nombreCiudad['ciudad']; ?></h1>
-				</div>
-				<div id="puntuacion">
-					<form method="get" action="php/controles/controlValoracionPais.php" id="valorPais">
-						<input type="hidden" value="<?php echo $pais;?>" name="pais"/>
-						<input type="hidden" value="<?php echo $userId;?>" name="userId"/>
-						<?php for($i=0;$i<10;$i++){ ?>
-						<img src="img/226.png" alt="Valoraci&oacute;n estrella"><input type="radio" name="valorCiudad" value="<?php echo $i+1;?>" class="valorCiudad" id="valorCiudad<?php echo $i+1;?>" title="Puntuaci&oacute;n">
-						<?php } ?>
-					</form>
-				</div>
-			</div>
-		</div> 
+	<span id="valoracionCiudad"></span>
+	<form method="get" action="php/controles/controlValoracionCiudad.php" id="valorCiudad">
+	            <input type="hidden" value="<?php echo $ciudad;?>" name="ciudad"/>
+	            <input type="hidden" value="<?php echo $userId;?>" name="userId"/>
+	            
+		      
+		      1<input type="radio" name="valorCiudad" value="1" class="valorCiudad" id="valorCiudad1">
+		      2<input type="radio" name="valorCiudad" value="2" class="valorCiudad" id="valorCiudad2">
+		      3<input type="radio" name="valorCiudad" value="3" class="valorCiudad" id="valorCiudad3">
+		      4<input type="radio" name="valorCiudad" value="4" class="valorCiudad" id="valorCiudad4">
+		      5<input type="radio" name="valorCiudad" value="5" class="valorCiudad" id="valorCiudad5">
+		      6<input type="radio" name="valorCiudad" value="6" class="valorCiudad" id="valorCiudad6">
+		      7<input type="radio" name="valorCiudad" value="7" class="valorCiudad" id="valorCiudad7">
+		      8<input type="radio" name="valorCiudad" value="8" class="valorCiudad" id="valorCiudad8">
+		      9<input type="radio" name="valorCiudad" value="9" class="valorCiudad" id="valorCiudad9">
+		      10<input type="radio" name="valorCiudad" value="10" class="valorCiudad" id="valorCiudad10">
+		</form>
+		<div id="cabecera"><img src="img/home_img1.jpg"> 
+		
+		</div>
 		<div id="menu-pais">
 			<ul>
-				<li><a href="#" id="informacion" title="Informaci&oacute;n">Informaci&oacute;n</a></li>
-				<li><a href="#" id="fotos" title="Galeria de imagenes">Fotos</a></li>
-				<li><a href="#" id="comentarios" title="Comentarios sobre la ciudad">Comentarios</a></li>
-				<li><a href="#" id="ubicacion" title="Ubicaci&oacute;n en el mapa">Ubicaci&oacute;n</a></li>
+				<li><a href="#" id="informacion">Informaci&oacute;n</a></li>
+				<li><a href="#" id="fotos">Fotos</a></li>
+				<li><a href="#" id="comentarios">Comentarios</a></li>
+				<li><a href="#" id="preguntas">Preguntas</a></li>
+				<li><a href="#" id="ubicacion">Ubicaci&oacute;n</a></li>
 			</ul>
 		</div>
 		<div id="caja-contenido">
 			<div id="contenido"></div>
 		</div>
-		<div id="bg-cuadro">
-			<div id="mapa-ubicacion">
-				<div class="cerrar-cuadro"><img src="img/75.png" alt="Cerrar cuadro" title="Cerrar"></div>
-			</div>
-		</div>
 	</div>
 	<div id="footer">
-		<span><a href="#" title="Informaci&oacute;n sobre nosotros">Sobre nosotros</a></span>
-		<span><a href="#" title="Condiciones de uso de la web">Condiciones</a></span>
-		<span><a href="#" title="Terminos de privacidad">Privacidad</a></span>
+		<span><a href="#">Sobre nosotros</a></span>
+		<span><a href="#">Condiciones</a></span>
+		<span><a href="#">Privacidad</a></span>
 		<span>&#64; 2014 Alumnos D.A.W.</span>
 	</div>
 </div>
