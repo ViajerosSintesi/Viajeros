3baeb3adfdc175423c1603e676e4454c8e6204ef
Lui86 <l.ruiz86@gmail.com>
2014-06-08 00:39:04 +0200

    dios que liooo

--- php/pais/pais-preguntas.php
+++ php/pais/pais-preguntas.php
@@ -2,45 +2,25 @@
 session_start();
 include("../funciones.php");
 $pais = "";
-
 if(isset($_GET['pais'])){
 	$pais = $_GET['pais'];
-
-	//$cursor = cargarComentCiudad($pais);
 	$session=1;
 	// falta terminaar de implemaentar la funcion.
 }
 ?>
 <!doctype html>
 <head>
-	<title></title>
-	<!--<script src="js/jquery-1.10.2.js"></script>
-	<script src="js/jquery-ui-1.10.4.custom.js"></script>-->
-	<style>
-	.imgperfil{
-	      width:50px;
-	      border-radius:20px;
-	}
-	.respuesta{
-	      margin-left: 10%;
-	}
-	</style>
+	<title>Preguntas</title>
+	<link rel="stylesheet" type="text/css" href="css/preguntas-style.css">
 	<script src="js/cargaScript.js"></script>
 	<script src="js/valoracion.js"></script>
-	
 	<script type="text/javascript">
-            $(function(){
-                  cargarPreguntas('<?php echo $pais;?>', '<?php echo $_SESSION['userId'];?>', 'Pais');
-                  
-            });
-            
-	      
+	$(function(){
+		cargarPreguntas('<?php echo $pais;?>', '<?php echo $_SESSION['userId'];?>', 'Pais');
+	});
 	</script>
 </head>
 <body>
-
-<div id="preguntas-pais">
-
-</div>
+<div id="preguntas-pais"></div>
 </body>
 </html>
